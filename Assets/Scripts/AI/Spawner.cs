a83fb64d844f365f98ccb3466008b46ae503caf0
Daniel <dwm.kiwi@gmail.com>
2021-06-13 11:53:12 +1200

    Add very basic spawning system

--- Assets/Scripts/AI/Spawner.cs
+++ Assets/Scripts/AI/Spawner.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private float spawnInterval = 10;
+    public GameObject[] spawnList;
+    public GameObject player;
+    private Transform playerTransform;
+    private MainBlock playerMainBlock;
     
     
     private float spawnCooldown = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTransform = player.GetComponent<Transform>();
+        playerMainBlock = player.GetComponent<MainBlock>();
     }
 
     // Update is called once per frame
@@ -24,9 +29,22 @@ public class Spawner : MonoBehaviour
             SpawnStuff();
         }
     }
-
+    
     void SpawnStuff()
     {
-        // Instantiate()
+        var idx = Random.Range(0, spawnList.Length);
+        var offset = Random.insideUnitCircle.normalized * 10;
+        var newObject = Instantiate(
+            spawnList[idx],
+            offset + (Vector2)playerTransform.position,
+            Quaternion.Euler(0.0f, 0.0f, Random.Range(0, 360))
+        );
+
+        var aiController = newObject.GetComponent<AiController>();
+        if (aiController != null)
+        {
+            var difficulty = playerMainBlock.GetDifficulty();
+            aiController.StartSpawnSequence(difficulty);
+        }
     }
 }
