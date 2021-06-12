0a4c9ae5b1331d996f38e756083e35fa0f09b3f1
EonZeNx <sparck.va@gmail.com>
2021-06-13 11:11:11 +1200

    WIP first pass - World limits

---
+++ Assets/Scripts/World/World.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    [Header("World Limits")]
+    public Transform wallNorth;
+    public Transform wallSouth;
+    public Transform wallEast;
+    public Transform wallWest;
+    public int size = 2048;
+    
+    [Header("WorldSeed")]
+    public bool useRandomSeed;
+    public int seed;
+    
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Generate();
+    }
+
+    public void Generate()
+    {
+        if (useRandomSeed)
+        {
+            seed = Random.Range(-100000, 100000);
+        }
+        
+        Random.InitState(seed);
+
+        var halfSize = (float) size / 2;
+        
+        wallNorth.position = new Vector3(0f, halfSize);
+        wallSouth.position = new Vector3(0f, -halfSize);
+        wallEast.position = new Vector3(halfSize, 0f);
+        wallWest.position = new Vector3(-halfSize, 0f);
+
+        wallNorth.localScale = new Vector3(size, 64f, 1f);
+        wallSouth.localScale = new Vector3(size, 64f, 1f);
+        wallEast.localScale = new Vector3(size, 64f, 1f);
+        wallWest.localScale = new Vector3(size, 64f, 1f);
+    }
+}
