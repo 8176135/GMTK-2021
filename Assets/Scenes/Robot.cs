e61e971f9601d7c89ea0bc2a2c66fc2554483285
Rex <elsecaller@8176135.xyz>
2021-06-12 14:20:50 +1200

    Some changes

---
+++ Assets/Scenes/Robot.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    private GameObject[] listOfConnectedObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
