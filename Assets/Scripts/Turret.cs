8b68e43e434e1e27fb6eab7730ecba99adc3dbb4
Daniel <dwm.kiwi@gmail.com>
2021-06-12 22:30:56 +1200

    Start on turret

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    public Transform rendererTransform;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void Aim(Vector2 target)
+    {
+        var rotation = Vector2.SignedAngle(Vector2.up, target - new Vector2(transform.localPosition.x, transform.localPosition.y));
+        rendererTransform.localRotation = Quaternion.RotateTowards(rendererTransform.localRotation, Quaternion.Euler(0,0,rotation), 4f);
     }
 
     // Update is called once per frame
